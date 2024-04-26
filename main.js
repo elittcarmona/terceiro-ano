f59ccb874f80c3a292602f242a3802f9b3073600
elittcarmona <132077265+elittcarmona@users.noreply.github.com>
2024-04-26 08:35:00 -0300

    .

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-04-28-30T00:00:00");
 const tempoObjetivo2 = new Date("2025-04-26-13T14:30:00");
 const tempoObjetivo3 = new Date("2024-12-30T13:20:00");
-const tempoObjetivo4 = new Date("2025-02-01T18:00:00");
+const tempoObjetivo4 = new Date("2024-07-01-01T18:00:00");
 
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
